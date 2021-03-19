0c21b38c8a240619213df2c512aa4b71021477fd
Leeseonha <jmtkdsh@gmail.com>
2021-03-20 00:39:09 +0900

    number 자료형

---
+++ node1/chap03/number1.js
@@ -0,0 +1,6 @@
+// 정수 실수 구분이 없다
+let a = 100;
+console.log(a);
+
+a = 3.14159;
+console.log(a);
---
+++ node1/chap03/toString.js
@@ -0,0 +1,10 @@
+// 형변환 (number -> string)
+let a = 123.456;
+
+let s = a.toString();
+console.log(s);
+console.log(typeof s);
+
+s = String(a);
+console.log(s);
+console.log(typeof s);
