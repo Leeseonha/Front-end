bc87e8f301d14d09aaaa672b0c81c331f19b1389
Leeseonha <jmtkdsh@gmail.com>
2021-03-19 22:51:54 +0900

    hello world

---
+++ node1/chap01/hello2.js
@@ -0,0 +1,3 @@
+// let 키워드로 변수 선언 // 지역변수를 선언할 때 var이 아니고 let을 사용해야 바람직하다.
+let message = "hello world";
+console.log(message);
---
+++ node1/chap01/hello3.js
@@ -0,0 +1,3 @@
+// var 키워드로 변수 선언
+var message = "hello world";
+console.log(message);
