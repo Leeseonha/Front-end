9c79e50e3a307d2e733636cff7ae74120913f742
Leeseonha <jmtkdsh@gmail.com>
2021-03-19 23:36:25 +0900

    변수 선언

---
+++ node1/chap02/err2.js
@@ -0,0 +1,8 @@
+// "use strict";        : 엄격한 문법은 사용해라
+// javascript 소스 코드 파일 선두에 "use strict"; 선언이 있으면, 이 소스 파일에서는 변수를 미리 선언하지 않고 사용할 경우에 문법 오류가 발생한다.
+// 이 파일은 실행되지 않는다
+
+"use strict";
+
+for (i = 0; i < 5; ++i)
+    console.log(i);
