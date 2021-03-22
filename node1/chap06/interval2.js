a620daca6773f812809276efffe00bbd68102612
Leeseonha <jmtkdsh@gmail.com>
2021-03-23 00:16:02 +0900

    타이머

---
+++ node1/chap06/interval2.js
@@ -0,0 +1,4 @@
+// setInteraval 함수
+setInterval(function (msg) {
+    console.log(msg, new Date());
+  }, 1000, "1초 간격");  
---
+++ node1/chap06/interval3.js
@@ -0,0 +1,2 @@
+// setInteraval 함수
+setInterval((msg) => console.log(msg, new Date()), 1000, "1초 간격");
