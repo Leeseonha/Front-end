fe6bd9db60e5799d9d9bb7de8add80c3b8f6969b
Leeseonha <jmtkdsh@gmail.com>
2021-04-14 02:28:20 +0900

    07 배열2 연습문제

---
+++ node1/chap08/personhw1.js
@@ -0,0 +1,18 @@
+// 이 배열을 age 내림차순으로 정렬하여 출력하는 코드를 구현하라.
+
+let persons = [
+    { name: "홍길동", age: 16 },
+    { name: "임꺽정", age: 18 },
+    { name: "전우치", age: 19 }
+];
+  
+persons.sort((p1, p2) => p2.age - p1.age);
+console.log(persons);
+
+
+// 출력
+// [
+//     { name: '전우치', age: 19 },
+//     { name: '임꺽정', age: 18 },
+//     { name: '홍길동', age: 16 }
+// ]
