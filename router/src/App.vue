e9a3496f0eb8ab9c90f06bae22a4659a8522bdb7
Leeseonha <jmtkdsh@gmail.com>
2021-06-12 21:23:52 +0900

    Vue Router 프로젝트

--- router/src/App.vue
+++ router/src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>router</h1>
+    <div class="menu">
+      <router-link to="/">홈</router-link>
+      <router-link to="/notice">공지사항</router-link>
+      <router-link to="/todo">할일</router-link>
+    </div>
+    <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueRouter from 'vue-router'
+import Home from './Home.vue'
+import Notice from './Notice.vue'
+import Todo from './Todo.vue'
+
+var router = new VueRouter({
+  routes: [
+    { path: '/', component: Home },
+    { path: '/notice', component: Notice },
+    { path: '/todo', component: Todo }
+  ]
+})
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  router
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+div#app { margin: 30px auto; max-width: 700px; min-height: 600px;
+  border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; }
+h1 {  background:linear-gradient(90deg, #def, #bcd); margin: 0; padding: 10px 30px; }
+div.menu { background-color: #eee;  }
+div.menu a { display: inline-block; padding: 15px 20px; font-size: 14pt;
+  text-decoration: none; color: black; }
+a.router-link-exact-active { background-color: #ddd; }
+.view { padding: 30px; }
 </style>
