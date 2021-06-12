9d1807f5ca092a9908fd0041719f6074601871df
Leeseonha <jmtkdsh@gmail.com>
2021-06-12 18:11:50 +0900

    Counter2 컴포넌트

--- vue_app22/src/App.vue
+++ vue_app22/src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>페이지2</h1>
+    <Counter2 title="카운터2" description="첫째 컴포넌트" color="#ffb"  v-on:send="showValue" />
+    <Counter2 title="카운터2" color="#afa"  v-on:send="showValue" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter2 from './Counter2.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  methods: {
+    showValue(value) {
+      alert(value);
+    }
+  },
+  components: { Counter2 }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+div#app { padding: 0 30px 30px 30px; margin: 30px auto; max-width: 400px;
+      border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; }
 </style>
